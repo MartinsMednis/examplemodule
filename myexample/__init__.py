6be1b17575094c151047a83724a90566eaad5c02
Mārtiņš Mednis <mrt@mednis.info>
2020-12-10 14:53:04 +0200

    First commit

---
+++ myexample/__init__.py
@@ -0,0 +1,8 @@
+"""
+myexample.
+An example python library.
+"""
+
+__version__ = "0.0.1"
+__author__ = "Martins Mednis"
+__credits__ = "Riga Coding School"
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='myexample',
+    version='0.1.0',
+    description='An example Python package',
+    url='https://github.com/MartinsMednis/examplemodule',
+    author='MartinsM',
+    author_email='martins@mednis.info',
+    license='GPL3',
+    packages=['myexample'],
+    install_requires=[]
+)
